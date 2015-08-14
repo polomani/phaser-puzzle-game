93f3220c3f238154fbcbbc2432e4d24d129daebd
Ivano Polomani <ivano.polomani@gmail.com>
2015-08-14 21:11:05 +0300

    zoom in editor

--- js/editor.js
+++ js/editor.js
@@ -15,12 +15,21 @@ Puzzle.Editor.prototype.preload = function () {
 	o.scale.setResizeCallback(onResizedEditor);
 	o.scale.refresh();
 	o.inputEnabled = true;
-	o.BSIZE = 50;
 };
 
 
-function onResizedEditor (f) {
-	
+function onResizedEditor () {
+	var xy = o_getMinMaxBoxesXY();
+	var w = (xy.max.x - xy.min.x)/o.BSIZE, h = (xy.max.y - xy.min.y)/o.BSIZE;
+	var indentX = Math.floor((window.innerWidth - w*o.BSIZE)/o.BSIZE/2);
+	var indentY = Math.floor((window.innerHeight - h*o.BSIZE)/o.BSIZE/2);
+	if (indentX < 0)
+		indentX = 0;
+	if (indentY < 0)
+		indentY = 0;
+	o.boxes.forEach (function (box) {o_resizeBox(box, indentX, indentY);})
+
+	o_updateNet();;
 }
 
 function onBoxDown(sprite, pointer) {
@@ -68,32 +77,36 @@ function changeCursor (key) {
 		
 		break;
 	}
-	if (cursor) cursor.type = key;
+	if (cursor) {
+		cursor.type = key;
+		cursor.scale.setTo (o.BSIZE/50, o.BSIZE/50);
+	}
 }
 
 Puzzle.Editor.prototype.create = function () {
+	//alert (levelsToString(LEVELS[Editor.aimLVL]));
 	o.world.setBounds(0, 0, 2880, 2880);
 	o.input.addMoveCallback(mouseMove);
 	o.input.onDown.add(mouseClicked);
 	o.boxes = o.add.group();
-	o.BSIZE = 50;
+	o.net = o.add.bitmapData(o.world.width,o.world.height);
+	o.add.sprite(0, 0, o.net);
+	o.BSIZE = 30;
 
 	o.ONE = o.input.keyboard.addKey(Phaser.Keyboard.ONE);
 	o.TWO = o.input.keyboard.addKey(Phaser.Keyboard.TWO);
 	o.THREE = o.input.keyboard.addKey(Phaser.Keyboard.THREE);
 	o.FOUR = o.input.keyboard.addKey(Phaser.Keyboard.FOUR);
 	o.keyS = o.input.keyboard.addKey(Phaser.Keyboard.S);
+	o.keyPlus = o.input.keyboard.addKey(Phaser.Keyboard.NUMPAD_ADD);
+	o.keyMinus = o.input.keyboard.addKey(Phaser.Keyboard.NUMPAD_SUBTRACT);
 	o.ONE.onDown.add(changeCursor);
 	o.TWO.onDown.add(changeCursor);
 	o.THREE.onDown.add(changeCursor);
 	o.FOUR.onDown.add(changeCursor);
 	o.keyS.onDown.add(commitLevel);
-
-	o.net = o.add.bitmapData(o.world.width,o.world.height);
-	
-	o.add.sprite(0, 0, o.net);
-	o_updateNet();
-	
+	o.keyMinus.onDown.add(function() {o_zoom(false);});
+	o.keyPlus.onDown.add(function() {o_zoom(true);});
 
 	var arr = LEVELS[Editor.aimLVL];
 	for (var y = 0; y < arr.length; y++) {
@@ -113,6 +126,8 @@ Puzzle.Editor.prototype.create = function () {
 				box.scale.setTo (o.BSIZE/50, o.BSIZE/50);
 				box.x = x*o.BSIZE;
 				box.y = y*o.BSIZE;
+				box.inputEnabled = true;
+				box.events.onInputDown.add(onBoxDown);
 			}
 		}
 	}
@@ -134,6 +149,38 @@ Puzzle.Editor.prototype.addMenu = function () {
 	game.add.text(0, 50, '1 = box black \n2 = box blue \n3 = box gap\n4 = cursor\nS = save lvl \n ', { font: '18px Arial', fill: '#0' });
 
 
+}
+
+o_zoom = function(plus) {
+	var xy = o_getMinMaxBoxesXY();
+	var w = (xy.max.x - xy.min.x)/o.BSIZE, h = (xy.max.y - xy.min.y)/o.BSIZE;
+
+	if (plus && o.BSIZE < 95) {
+		o.BSIZE += 5;
+	} else if (!plus && o.BSIZE > 10){
+		o.BSIZE -= 5;
+	}
+	o_updateNet();
+
+	var indentX = Math.floor((window.innerWidth - w*o.BSIZE)/o.BSIZE/2);
+	var indentY = Math.floor((window.innerHeight - h*o.BSIZE)/o.BSIZE/2);
+	if (indentX < 0)
+		indentX = 0;
+	if (indentY < 0)
+		indentY = 0;
+	o.boxes.forEach (function (box) {o_resizeBox(box,indentX, indentY);})
+	if (cursor) {
+
+		cursor.scale.setTo (o.BSIZE/50, o.BSIZE/50);
+	}
+}
+
+function o_resizeBox (box, indentX, indentY) {
+	box.scale.setTo (o.BSIZE/50, o.BSIZE/50);
+	var xx = (indentX + box.indexX) * o.BSIZE;
+	var yy = (indentY + box.indexY)* o.BSIZE;
+	box.x = xx;
+	box.y = yy;
 }
 
 function commitLevel() {
@@ -191,32 +238,48 @@ function levelToString (arr) {
 	return string;
 }
 
-function saveLevel () {
-	if (o.boxes.length == 0) return null;
+function o_getMinMaxBoxesXY () {
 	var minX = o.boxes.getChildAt(0).x, minY = o.boxes.getChildAt(0).y, maxX = 0, maxY = 0;
 	o.boxes.forEach (function(box) {
-		if (box == cursor) return; 
+		if (box == cursor) return;
 		if (box.x > maxX) maxX = box.x;
 		if (box.y > maxY) maxY = box.y;
 		if (box.x < minX) minX = box.x;
 		if (box.y < minY) minY = box.y;
 	});
+	return {
+		min:{
+			x:minX,
+			y:minY
+		},
+		max:{
+			x:maxX,
+			y:maxY
+		}
+	};
+}
+
+function saveLevel () {
+	if (o.boxes.length == 0) return null;
+	var xy = o_getMinMaxBoxesXY();
+	var minX = xy.min.x, minY = xy.min.y, maxX = xy.max.x, maxY = xy.max.y;
+
 	minX /= o.BSIZE;
 	minY /= o.BSIZE;
 	maxX /= o.BSIZE;
 	maxY /= o.BSIZE;
-	var levelArr = new Array (maxX - minX + 1);
-	//alert (maxX - minX + 1);
-	for (var i = 0; i < levelArr.length; ++i) {
-		levelArr[i] = new Array (maxY - minY + 1);
-		for (var j = 0; j < levelArr[i].length; ++j) {
-			levelArr[i][j] = 0;
+	var sizeX = maxX - minX + 1;
+	var sizeY = maxY - minY + 1;
+	var levelArr = new Array (sizeY);
+	for (var i = 0; i < sizeY; ++i) {
+		levelArr[i] = new Array (sizeX);
+		for (var j = 0; j < sizeX; ++j) {
+			 levelArr[i][j] = 0;
 		}
 	}
 	o.boxes.forEach (function(box) {
 		if (box == cursor) return;
-		//alert (levelArr.length + " " + box.x/o.BSIZE);
-		levelArr [box.x/o.BSIZE - minX][box.y/o.BSIZE - minY] = box.btype;
+		levelArr [box.y/o.BSIZE - minY][box.x/o.BSIZE - minX] = box.btype;
 	});
 	var str = "[";
 	for (var i = 0; i < levelArr.length; ++i) {
@@ -252,9 +315,6 @@ Puzzle.Editor.prototype.update = function() {
 	{
 		o.camera.x += o.BSIZE;
 	}
-	if (cursors.up.isDown || cursors.down.isDown || cursors.left.isDown || cursors.right.isDown) {
-		o_updateNet();
-	}
 };
 
 Puzzle.Editor.prototype.render = function() {
@@ -263,15 +323,16 @@ Puzzle.Editor.prototype.render = function() {
 
 
 o_updateNet = function () {
+	o.net.ctx.clearRect(0, 0, o.net.width, o.net.height);
 	o.net.ctx.beginPath();
 	o.net.ctx.lineWidth = "1";
 	o.net.ctx.strokeStyle = 0;
 	o.net.ctx.moveTo(0, 0);
-	for (var j = 0; j < (height+o.camera.y)/o.BSIZE; ++j) {
+	for (var j = 0; j < o.world.height/o.BSIZE; ++j) {
 		o.net.ctx.moveTo(0, j*o.BSIZE);
 		o.net.ctx.lineTo(o.world.width, j*o.BSIZE);
 	}
-	for (var j = 0; j < (width+o.camera.x)/o.BSIZE; ++j) {
+	for (var j = 0; j < o.world.width/o.BSIZE; ++j) {
 		o.net.ctx.moveTo(j*o.BSIZE, 0);
 		o.net.ctx.lineTo(j*o.BSIZE, o.world.height);
 	}
