362ae7f2f1b71818dd0a0f1d1a8c6886158a20b1
Ivano Polomani <ivano.polomani@gmail.com>
2017-01-08 18:29:46 +0200

    new tutorial (almost done)

--- js/hints.js
+++ js/hints.js
@@ -3,8 +3,8 @@ Tutorial = function(){};
 Tutorial.resize = function() {
 	if (Tutorial.image) {
 		Tutorial.image.width = 
-		Tutorial.image.height = Math.min (Math.min(game.width, game.height)/3, Tutorial.image.realWidth);
-		Tutorial.image.x = game.width-Tutorial.image.width;
+		Tutorial.image.height = Math.min (Dimensions.getMinDimension()/3, Tutorial.image.realWidth);
+		Tutorial.image.x = game.width/2;
 	}
 	if (Tutorial.text) {
 		Tutorial.text.x = game.width/2;
@@ -14,10 +14,11 @@ Tutorial.resize = function() {
 
 Tutorial.open = function (level) {
 	
-	if (false && game.cache.checkImageKey('tutorial_'+level)) {
+	if (game.cache.checkImageKey('tutorial_'+level)) {
 		var animation = game.add.sprite(game.width-228/2, 0, 'tutorial_'+level);
 	    animation.animations.add('play');
-	    animation.animations.play('play', 13, true);
+	    animation.anchor.set (0.5, 0);
+	    animation.animations.play('play', 30, true);
 	    animation.realWidth=animation.width;
 	    animation.realHeight=animation.height;
 
